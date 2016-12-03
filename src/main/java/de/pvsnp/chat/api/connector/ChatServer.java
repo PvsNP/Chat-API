316e3ee5df09caf175964cf3b91322d12b7aa074
Letsplaybar <chsiemund@uni-potsdam.de>
2016-12-03 13:33:35 +0100

    netty.io server/client connect hinzugefügt

--- src/main/java/de/pvsnp/chat/api/connector/ChatServer.java
+++ src/main/java/de/pvsnp/chat/api/connector/ChatServer.java
@@ -5,6 +5,14 @@
  */
 package de.pvsnp.chat.api.connector;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,20 +23,41 @@ import lombok.Setter;
 public class ChatServer {
     
     private @Getter  @Setter int port;
-    private @Getter @Setter String ip;
     public @Getter @Setter boolean keepalive; 
     
-    public ChatServer(int port, String ip) {
+    public ChatServer(int port) {
         this.port = port;
-        this.ip = ip;
         this.keepalive = true;
     }
     
     public void connect(){
         EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
         EventLoopGroup workerGroup = new NioEventLoopGroup(); // (1)
+        
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap(); // (2)
+            bootstrap.group(bossGroup, workerGroup); // (3)
+            bootstrap.channel(NioServerSocketChannel.class);// (4)
+            bootstrap.childHandler(new ChannelInitializer<SocketChannel>() { // 5
+			@Override
+			protected void initChannel(SocketChannel channel) throws Exception {
+				System.out.println("Ein Computer hat sich verbunden. IP: " + channel.remoteAddress().getHostName()); // (6)
+			}
+            });// (7)
+            bootstrap.childOption(ChannelOption.SO_KEEPALIVE, keepalive); // (8)
+            ChannelFuture future = bootstrap.bind(port).sync(); // (9)
+            System.out.println("Server gestartet!");
+            future.channel().closeFuture().sync(); // (10)
+            } catch (Exception ex) {
+		ex.printStackTrace();
+            } finally {
+		bossGroup.shutdownGracefully(); // (11)
+                workerGroup.shutdownGracefully(); // (11)
+            }
+	}
     }
+
     
     
     
-}
+
