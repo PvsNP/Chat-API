e6f69cb2e025a26166fcaabb5d3d29167ac694a6
Letsplaybar <chsiemund@uni-potsdam.de>
2016-12-03 12:57:56 +0100

    config ausgebaut

--- src/main/java/de/pvsnp/chat/api/main/ChatAPI.java
+++ src/main/java/de/pvsnp/chat/api/main/ChatAPI.java
@@ -6,10 +6,6 @@
 package de.pvsnp.chat.api.main;
 
 import de.pvsnp.chat.mongodb.MongoManager;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -29,28 +25,13 @@ public class ChatAPI {
         return instance;
     }
     private void load(){
-       
+       registerMongoDB();
+        
     }
     
-     public void registerMongoDB(){
-        File file = new File("Configs/","MongoDB.yml");
-        FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
-        cfg.options().copyDefaults(true);
-        cfg.addDefault("IP", "localhost");
-        cfg.addDefault("Port", 27017);
-        cfg.addDefault("Datenbank", "Minecraft");
-        cfg.addDefault("User", "User");
-        cfg.addDefault("Password", "Password");
-        cfg.addDefault("AuthDatabase", "admin");
-        
-            try {
-                cfg.save(file);
-            } catch (IOException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-         manager = new MongoManager(cfg.getString("IP"), cfg.getInt("Port"), cfg.getString("Datenbank"),cfg.getString("User"),cfg.getString("Password"), cfg.getString("AuthDatabase"));
-		manager.connect();
+     public void registerMongoDB(){      
+        manager = new MongoManager("127.0.0.1", 27017, "Chatapi","Letsplaybar","mama40", "admin");
+        manager.connect();
     }
      
      public MongoManager getManager(){
